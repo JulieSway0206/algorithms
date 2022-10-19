09e5d79b81a5a1339a0d961ff6b35895252ad029
jie.cui <jie.cui@servicenow.com>
2022-10-19 11:15:25 -0700

    update with approach name

--- linked-list/loop-detection/142.Linked-List-Cycle-II.java
+++ linked-list/loop-detection/142.Linked-List-Cycle-II.java
@@ -2,6 +2,7 @@
 //There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
 //Time complexity : O(n)
 //Space complexity : O(1)
+//Approach: Floyd's Tortoise and Hare
 public class Solution {
     private ListNode getIntersect(ListNode head) {
         ListNode tortoise = head;
