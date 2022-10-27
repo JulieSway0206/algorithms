51e74ad59db06477b9fb3429e94c7ff724be2c28
jie.cui <jie.cui@servicenow.com>
2022-10-26 22:22:46 -0700

    search in rotated array

---
+++ sorting-and-searching/search-in-rotated-array/33.Search-in-Rotated-Sorted-Array.java
@@ -0,0 +1,112 @@
+// There is an integer array nums sorted in ascending order (with distinct values).
+// Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+// Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+// You must write an algorithm with O(log n) runtime complexity.
+// Exp:
+// Input: nums = [4,5,6,7,0,1,2], target = 0
+// Output: 4
+
+// Input: nums = [4,5,6,7,0,1,2], target = 3
+// Output: -1
+
+// Approach 1: Binary search
+//Case 1:
+// Pivot element is larger than the first element in the array, i.e. the subarray from the first element to the pivot is non-rotated.
+// If the target is located in the non-rotated subarray:
+//   go left: `end = mid - 1`.
+// Otherwise: go right: `start = mid + 1`.
+
+//Case 2:
+// Pivot element is smaller than the first element of the array, i.e. the rotation index is somewhere between 0 and mid. It implies that the sub-array from the pivot element to the last one is non-rotated.
+// If the target is located in the non-rotated subarray:
+//   go right: `start = mid + 1`.
+// Otherwise: go left: `end = mid - 1`.
+
+// Time complexity : O(logN).
+// Space complexity : O(1).
+class Solution {
+  int [] nums;
+  int target;
+
+  public int find_rotate_index(int left, int right) {
+    if (nums[left] < nums[right])
+      return 0;
+
+    while (left <= right) {
+      int pivot = (left + right) / 2;
+      if (nums[pivot] > nums[pivot + 1])
+        return pivot + 1;
+      else {
+        if (nums[pivot] < nums[left])
+          right = pivot - 1;
+        else
+          left = pivot + 1;
+      }
+    }
+    return 0;
+  }
+
+  public int search(int left, int right) {
+    /*
+    Binary search
+    */
+    while (left <= right) {
+      int pivot = (left + right) / 2;
+      if (nums[pivot] == target)
+        return pivot;
+      else {
+        if (target < nums[pivot])
+          right = pivot - 1;
+        else
+          left = pivot + 1;
+      }
+    }
+    return -1;
+  }
+
+  public int search(int[] nums, int target) {
+    this.nums = nums;
+    this.target = target;
+
+    int n = nums.length;
+
+    if (n == 1)
+      return this.nums[0] == target ? 0 : -1;
+
+    int rotate_index = find_rotate_index(0, n - 1);
+
+    // if target is the smallest element
+    if (nums[rotate_index] == target)
+      return rotate_index;
+    // if array is not rotated, search in the entire array
+    if (rotate_index == 0)
+      return search(0, n - 1);
+    if (target < nums[0])
+      // search in the right side
+      return search(rotate_index, n - 1);
+    // search in the left side
+    return search(0, rotate_index);
+  }
+}
+
+// Approach 2: One-pass Binary Search
+// Time complexity: O(logN).
+// Space complexity: O(1).
+class Solution {
+  public int search(int[] nums, int target) {
+    int start = 0, end = nums.length - 1;
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      if (nums[mid] == target) return mid;
+      else if (nums[mid] >= nums[start]) {
+        if (target >= nums[start] && target < nums[mid]) end = mid - 1;
+        else start = mid + 1;
+      }
+      else {
+        if (target <= nums[end] && target > nums[mid]) start = mid + 1;
+        else end = mid - 1;
+      }
+    }
+    return -1;
+  }
+}
