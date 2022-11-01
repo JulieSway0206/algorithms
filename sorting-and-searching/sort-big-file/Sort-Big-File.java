3107c86e3a543b5fcfb897806f0fea8add0d1184
jie.cui <jie.cui@servicenow.com>
2022-10-31 22:02:16 -0700

    sort big file

---
+++ sorting-and-searching/sort-big-file/Sort-Big-File.java
@@ -0,0 +1,3 @@
+//Imagine you have a 20 GB file with one string per line. Explain how you would sort the file
+//Approach: External sort
+https://www.geeksforgeeks.org/sorting-larger-file-with-smaller-ram/
